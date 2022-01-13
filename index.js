ddf9bf329628747f02104d27481d753504a4d6da
Schlabauke <annabelle.jeffre@gmail.com>
2022-01-13 13:50:50 +0100

    Projekt MovieNight

---
+++ index.js
@@ -0,0 +1,14 @@
+/* Was soll passieren:
+Templates erstellen:
+    ->Navbar,
+    ->MovieKärtchen
+    ->MovieDetail
+
+Routen definieren
+-> datenstruktur anschauen
+ ->fetch machen mit axion
+ -> daten ausgeben lassen
+-> Template mit Beispiel-Daten füllen
+-> Fetch-Daten in Templ einfügen
+-> error definieren
+*/
